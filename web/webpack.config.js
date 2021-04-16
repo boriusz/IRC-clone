674e095b05b1a123095954c7815ecabbfdd77a85
Boriusz <r.paliwoda1928@gmail.com>
2021-04-16 20:58:17 +0200

    Initial commit.

---
+++ web/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devtool: "inline-source-map",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 4000,
+    watchContentBase: true,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  context: path.join(__dirname),
+};
